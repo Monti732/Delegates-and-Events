bea01d1c2a18b9a9e7c617e43de04bb56ce1f744
Monti732 <viktorsmakov073@gmail.com>
2025-04-09 00:44:13 +0700

    Add extension methods to SquareMatrix.cs class

--- Delegates and Events/Program.cs
+++ Delegates and Events/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿namespace Delegates_and_Events;
+
+class Program {
+  static void Main() {
+    var matrix = new SquareMatrix(3,0);
+    Console.WriteLine(matrix);
+    Console.WriteLine($"\n{matrix.Transpose()}\n");
+    Console.WriteLine(matrix.FindTrace());
+    Console.WriteLine($"\n{matrix.ToDiagonalForm()}");
+  }
+}
---
+++ Delegates and Events/SquareMatrixExtensionMethods.cs
@@ -0,0 +1,35 @@
+﻿namespace Delegates_and_Events;
+
+public static class SquareMatrixExtensionMethods {
+    public static SquareMatrix Transpose(this SquareMatrix matrix) {
+        SquareMatrix transposeMatrix = new SquareMatrix(matrix);
+        for (int row = 0; row < matrix.Size; ++row) {
+            for (int col = 0; col < matrix.Size; ++col) {
+                transposeMatrix.Matrix[row, col] = matrix.Matrix[col, row];
+            }
+        }
+
+        return transposeMatrix;
+    }
+    
+    public static double FindTrace(this SquareMatrix matrix) {
+        double trace = 0;
+        for (int row = 0; row < matrix.Size; ++row) {
+            trace += matrix.Matrix[row, row];
+        }
+
+        return trace;
+    }
+    
+    public static SquareMatrix ToDiagonalForm(this SquareMatrix matrix) {
+        for (int row = 0; row < matrix.Size; ++row) {
+            for (int col = 0; col < matrix.Size; ++col) {
+                if (row != col) {
+                    matrix.Matrix[row, col] = 0;
+                }
+            }
+        }
+
+        return matrix;
+    }
+}
